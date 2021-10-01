c05631ce921103569369719e977259e13ea78abc
SANKALP <tigerishan426@gmail.com>
2021-10-01 20:55:40 +0530

    Log View Controller File Added , Minor Changes.

---
+++ Beverages/Controller/LogViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LogViewController.swift
+//  Beverages
+//
+//  Created by Sankalp Pandey on 01/10/21.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class LogViewController: UIViewController{
+    
+}
