2591a4bc1e8f45474345f314af7ebf7a766158ea
SANKALP1011 <tigerishan426@gmail.com>
2021-10-10 22:16:38 +0530

    Sign in test passed , error solved using pods.

--- Beverages/Controller/SignViewController.swift
+++ Beverages/Controller/SignViewController.swift
@@ -11,4 +11,19 @@ import Firebase
 
 class SignViewController: UIViewController{
     
+    @IBOutlet var yourEmail: UITextField!
+    @IBOutlet var yourPassword: UITextField!
+    @IBOutlet var signButton: UIButton!
+    
+    @IBAction func signButton(_ sender: UIButton!){
+        Firebase.Auth.auth().createUser(withEmail: yourEmail.text!, password: yourPassword.text!) { (authResult , error) in
+            if error == nil{
+                print(error?.localizedDescription)
+            }
+            else{
+                print("signed in")
+            }
+        }
+    }
+    
 }
