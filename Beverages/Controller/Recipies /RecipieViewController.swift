c70cef02f2282f5893b7ded39356438f9839c00a
SANKALP1011 <tigerishan426@gmail.com>
2021-10-28 01:39:27 +0530

    Recipe View controller added.

---
+++ Beverages/Controller/Recipies /RecipieViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RecipieViewController.swift
+//  Beverages
+//
+//  Created by Sankalp Pandey on 28/10/21.
+//
+
+import Foundation
+import UIKit
+
+class RecipieViewController: UIViewController{
+    
+    
+}
